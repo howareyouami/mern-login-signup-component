2bf22b883cb4ec9964933a9e4929b403f3f76212
amal jose <amal.jose@appsintegra.com>
2020-03-19 09:22:25 +0530

    Update schema

--- models/User.js
+++ models/User.js
@@ -3,19 +3,23 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
   },
   email: {
-    type: String,
-    required: true
-  },
-  password: {
     type: String,
     required: true
   },
   date: {
     type: Date,
     default: Date.now
+  },
+  otp: {
+    type: String,
+  },
+  otpGeneratedTime: {
+    type: Date,
+  },
+  isRegistered: {
+    type: Boolean,
   }
 });
 
