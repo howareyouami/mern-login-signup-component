c607782c087a76f6327a2a0ce256c90c264092b1
amal jose <amal.jose@appsintegra.com>
2020-03-20 10:54:32 +0530

    Add console for debugging

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
-const { DB_USERNAME,DB_PASSWORD,DB_CLUSTER } = require("./config");
+const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER } = require("./config");
 
+console.log("DB_USERNAME===>", DB_USERNAME, "DB_PASSWORD", DB_PASSWORD, "DB_CLUSTER", DB_CLUSTER)
 module.exports = {
   MongoURI: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`
 };
